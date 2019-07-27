cc9bb237792094421dfa3a3bdb31f444cafba596
splean101 <40311839+splean101@users.noreply.github.com>
2019-07-27 12:09:03 +0300

    fin

--- src/index.js
+++ src/index.js
@@ -1,28 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 import createHistory from 'history/createBrowserHistory';
 
 const history = createHistory();
 
-const First = () => (
-    <div>
-        <h2>first component</h2>
-    </div>
-);
+const First = () => (<div><h2>First component</h2></div>);
+const Second = () => (<div><h2>Second component</h2></div>);
+const Third = () => (<div><h2>Third component</h2></div>);
 
-const First = () => (
-    <div>
-        <h2>first component</h2>
-    </div>
-);
+const Navigation = () => {
+    return <Router>
+        <div>
+            <ul>
+                <li> <NavLink to='/first'>first</NavLink></li>
+                <li> <NavLink to='/second'>second</NavLink></li>
+                <li> <NavLink to='/third'>third</NavLink></li>
+            </ul>
+            <Route exact path='/first' component={Second}></Route>
+            <Route exact path='/second' component={Third}></Route>
+            <Route exact path='/third' component={First}></Route>
+        </div>
+    </Router>
+}
 
-const First = () => (
-    <div>
-        <h2>first component</h2>
-    </div>
-);
-
-
-
-ReactDOM.render(<RList data={arr} />, document.getElementById("div1"));
+ReactDOM.render(<Navigation />, document.getElementById("div1"));
